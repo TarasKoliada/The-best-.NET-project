31d8dfcb0a69665e3d4934925f1bc63b77a327ee
Taras <kolyada.taras42@gmail.com>
2021-06-22 01:25:42 +0300

    Adjusted product removing. Moved logic to methods

--- dotNET_shop/Product.cs
+++ dotNET_shop/Product.cs
@@ -9,20 +9,116 @@ namespace dotNET_shop
 {
     class Product
     {
+        List<Product> list = new List<Product>();
         public string Name{ get; set; }
         public decimal Price { get; set; }
         public string Category { get; set; }
         public int Id { get; set; }
 
-       // public static int globalID;
-        public void showProduct()
+        private static int idIterator = 0;
+        public void errorMsg()
         {
-            //globalID++;
-            //id = Interlocked.Increment(ref globalID);
+            Console.WriteLine("\n!!!!!!!!!!!!!!!!!! Wrong Input Format !!!!!!!!!!!!!!!!!!");
+        }
+        public void showProductInfo()
+        {
             Console.WriteLine($"\nID: {Id}");
             Console.WriteLine($"Name: {Name}");
-            Console.WriteLine($"Price: {Price}");
+            Console.WriteLine($"Price: {Price}$");
             Console.WriteLine($"Category: {Category}");
+        }
+        public void showChoiseMenu()
+        {
+            Console.WriteLine("\nSelect an action: ");
+            Console.WriteLine("1) Get all prooducts.");
+            Console.WriteLine("2) Add Product.");
+            Console.WriteLine("3) Remove Product.");
+            Console.WriteLine("0) Exit");
+            Console.Write("Your choise: ");
+        }
+        public void getAllProducts()
+        {
+            if (list.Count == 0)
+            {
+                Console.WriteLine("\n---There are no products yet. Add them and come back here---");
+            }
+            else
+            {
+                foreach (Product item in list)
+                {
+                    item.showProductInfo();
+                }
+            }
+        }
+        public void addProduct()
+        {
+            string name, category, stringPrice, stringExit;
+            int exit = 0;
+            while (exit != 2)
+            {
+                Console.Clear();
+                Console.Write("Name: ");
+                name = Console.ReadLine();
+                Console.Write("Price: ");
+                stringPrice = Console.ReadLine();
+                if (decimal.TryParse(stringPrice, out decimal price))
+                {
+                    Console.Write("Category: ");
+                    category = Console.ReadLine();
+                    idIterator++;
+                    list.Add(new Product() { Name = name, Price = price, Category = category, Id = idIterator });
+                    
+                    Console.WriteLine("\n1) Add another one.");
+                    Console.WriteLine("2) Stop");
+                    Console.Write("Your choise: ");
+                    stringExit = Console.ReadLine();
+                    if (int.TryParse(stringExit, out exit) == false)
+                    {
+                        errorMsg();
+                        break;
+                    }
+                }
+                else
+                {
+                    errorMsg();
+                    break;
+                }
+            }
+        }
+        public void removeProduct()
+        {
+            if (list.Count != 0)
+            {
+                string stringId;
+                int productId;
+                Console.Write("\nEnter product ID: ");
+                stringId = Console.ReadLine();
+                if (int.TryParse(stringId, out productId))
+                {
+                    var deleteProduct = list.FirstOrDefault(prod => prod.Id == productId);
+                    if (deleteProduct == null)
+                    {
+                        Console.WriteLine($"!!!!The product with ID: {productId} not found!!!!"); ;
+                    }
+                    else
+                    {
+                        list.Remove(deleteProduct);
+                        Console.WriteLine($"\n---The Product with ID: {productId} has been removed---");
+                    }
+                }
+                else
+                {
+                    errorMsg();
+                }
+                if (list.Count == 0)
+                {
+                    idIterator = 0;
+                }
+            }
+            else
+            {
+                Console.WriteLine("\n---There are no Products to delete---");
+            } 
         }
     }
 }
--- dotNET_shop/Program.cs
+++ dotNET_shop/Program.cs
@@ -10,75 +10,29 @@ namespace dotNET_shop
     {
         static void Main(string[] args)
         {
-            List<Product> list = new List<Product>();
-           
-            int choise, exit, idIterator, productId;
-            exit = idIterator = 0;
-            string name, category;
-            decimal price;
-            int iterator = 1;
+            var product = new Product();
+            int choise;
             do
             {
-                Console.WriteLine("\nSelect an action: ");
-                Console.WriteLine("1) Get all prooducts.");
-                Console.WriteLine("2) Add Product.");
-                Console.WriteLine("3) Remove Product.");
-                Console.WriteLine("0) Exit");
-                Console.Write("Your choise: ");
+                product.showChoiseMenu();
                 choise = int.Parse(Console.ReadLine());
-
                 switch (choise)
                 {
                     case 1:
-                        if (list.Count == 0)
-                        {
-                            Console.WriteLine("\nThere are no products yet. Add them and come back here.");
-                            idIterator = 0;
-                        }
-                        else
-                        {
-                            foreach (Product item in list)
-                            {
-                                item.showProduct();
-
-                            }
-                        }
+                        product.getAllProducts();
                         break;
-
                     case 2:
-                        while (exit != 2)
-                        {
-                            Console.Clear();
-                            Console.Write("Name: ");
-                            name = Console.ReadLine();
-                            Console.Write("Price: ");
-                            price = int.Parse(Console.ReadLine());
-                            Console.Write("Category: ");
-                            category = Console.ReadLine();
-                            idIterator++;
-                             list.Add(new Product() { Name = name, Price = price, Category = category, Id = idIterator });
-
-                            if (iterator > 0)
-                            {
-                                Console.WriteLine("\n1) Add another one.");
-                                Console.WriteLine("2) Stop");
-                                Console.Write("Your choise: ");
-                                exit = int.Parse(Console.ReadLine());
-                                
-                            }
-                            iterator++;
-                        }
-                        exit = 0;
+                        product.addProduct();
                         break;
                     case 3:
-                        Console.Write("\nEnter product ID: ");
-                        productId = int.Parse(Console.ReadLine());
-                        productId -= 1;
-                        list.RemoveAt(productId);//the indexes of the list move down and happens OutOfRangeException
+                        product.removeProduct();
+                        break;
+                    default:
+                        product.errorMsg();
                         break;
                 }
             } while (choise != 0);
-            
+
         }
     }
 }
